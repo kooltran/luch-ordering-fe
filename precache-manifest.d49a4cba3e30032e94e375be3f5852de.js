bc1b74433035f63bb8097fb00517d1e6e19c9012
kooltran <kiettrankm11@gmail.com>
2020-09-03 22:48:03 +0700

    Updates

---
+++ precache-manifest.d49a4cba3e30032e94e375be3f5852de.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9781dcfd4a908e28b66398fb79602dd4",
+    "url": "/luch-ordering-fe/index.html"
+  },
+  {
+    "revision": "11006a9b06064b87dd6f",
+    "url": "/luch-ordering-fe/static/css/2.611f078b.chunk.css"
+  },
+  {
+    "revision": "c9ddce3b06f070818d6a",
+    "url": "/luch-ordering-fe/static/css/main.7e556900.chunk.css"
+  },
+  {
+    "revision": "11006a9b06064b87dd6f",
+    "url": "/luch-ordering-fe/static/js/2.310a1e1a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/luch-ordering-fe/static/js/2.310a1e1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9ddce3b06f070818d6a",
+    "url": "/luch-ordering-fe/static/js/main.a629d664.chunk.js"
+  },
+  {
+    "revision": "a88ffaf7ac03d71b433a",
+    "url": "/luch-ordering-fe/static/js/runtime-main.b7400314.js"
+  },
+  {
+    "revision": "890dce6082692bf7707393840ef73b85",
+    "url": "/luch-ordering-fe/static/media/cart.890dce60.svg"
+  },
+  {
+    "revision": "80d775c4046813ef4fbdd1945409e129",
+    "url": "/luch-ordering-fe/static/media/checklist.80d775c4.svg"
+  },
+  {
+    "revision": "211b8e656d46238cc51e806ab0ce13f5",
+    "url": "/luch-ordering-fe/static/media/congrats.211b8e65.svg"
+  },
+  {
+    "revision": "441d8611106ab6473ed8c5487cda63ac",
+    "url": "/luch-ordering-fe/static/media/cross.441d8611.svg"
+  },
+  {
+    "revision": "6b4b70dc5aad5de949394d10e5e15afa",
+    "url": "/luch-ordering-fe/static/media/fobiden-order.6b4b70dc.svg"
+  },
+  {
+    "revision": "546dec86ea5b592315528400b2ff808a",
+    "url": "/luch-ordering-fe/static/media/loading.546dec86.svg"
+  },
+  {
+    "revision": "913ae04a9c92f21844d794e2383928f6",
+    "url": "/luch-ordering-fe/static/media/logo.913ae04a.svg"
+  },
+  {
+    "revision": "566800df89cedb22bc3c01bdb30bc836",
+    "url": "/luch-ordering-fe/static/media/old_paper_bg.566800df.jpg"
+  },
+  {
+    "revision": "311313cded0dc23bba479b786ac0aad8",
+    "url": "/luch-ordering-fe/static/media/plus.311313cd.svg"
+  },
+  {
+    "revision": "db93b3b8239bfa0f8f8fc0ef0d2d25eb",
+    "url": "/luch-ordering-fe/static/media/tick.db93b3b8.svg"
+  }
+]);
